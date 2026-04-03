462aa7ee3abb3955000c454d1044c482ca1fe9b9
Lucas Ferreira <lukasnoobre53@gmail.com>
2026-04-03 17:29:11 -0300

    feat: create usecase for expoting csv from events

--- src/routes/event.py
+++ src/routes/event.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 from fastapi import APIRouter
 from typing import Literal
+from fastapi.responses import StreamingResponse
+
 
 from src.models import CreateEventDto
 
 from src.usecases.event import CreateEventUseCase
 from src.usecases.event import ListAllEventsUseCase
+from src.usecases.event import ExportCsvUseCase
 
 router = APIRouter(prefix="/events", tags=["events"])
 
@@ -17,3 +20,13 @@ def get_all_events(page: int = 1, per_page: int = 10):
 @router.post('')
 def create_event(createEventDto: CreateEventDto):
     return CreateEventUseCase().execute(createEventDto)
+
+@router.get("/csv")
+def export_csv():
+    return StreamingResponse(
+        ExportCsvUseCase().execute(),
+        media_type="text/csv",
+        headers={
+            "Content-Disposition": "attachment; filename=events.csv"
+        }
+    )
